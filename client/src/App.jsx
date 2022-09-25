22d25d956855c31e3b70cda869736eef823ddbd6
Tanmay Panda <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-09-25 03:27:51 +0000

    08:57

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+/* src/App.jsx */
+import React from "react";
+
+
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-blue-900">
+        REST
+      </h1>
+    </div>
+  );
+}
+
+export default App;
