476ea081ad3c554774e4f39f9305c5288bf82547
Tanmay Panda <87299490+uneconomicalfairy14@users.noreply.github.com>
2022-10-01 03:26:29 +0000

    08:56

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/orders';
+const url = 'http://localhost:5000/posts';
 
-export const fetchOrders =  () => axios.get(url);
+export const fetchOrders = () => axios.get(url);
+export const createOrder = (newPost) => axios.post(url, newPost);
+export const updateOrder = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
+export const deleteOrder = (id) => axios.delete(`${url}/${id}`);
--- client/src/reducer/index.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-import orders from './orders';
-
-export const reducers = combineReducers({ orders });
